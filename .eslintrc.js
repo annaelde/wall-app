0ec3c20c79ca62b0c4a443fbf7052de7262b01cc
annaelde <elde.av@gmail.com>
2018-08-11 15:20:29 -0400

    change function formatting.

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,8 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'indent': ['error', 4]
+    'indent': ['error', 4],
+    'space-before-function-paren': ['error', 'never']
   },
   parserOptions: {
     parser: 'babel-eslint'
