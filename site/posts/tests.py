5b829aebbba99b7dc8dd82271cb72b0e0d148e5f
annaelde <elde.av@gmail.com>
2018-08-12 17:25:14 -0400

    add unit tests for django project.

--- site/posts/tests.py
+++ site/posts/tests.py
@@ -1,3 +1,82 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.test import TestCase
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APIClient
+
+from users.models import User
+
+from .models import Post
+
+
+class PostTestCase(TestCase):
+    """ 
+    Testing that you must be authorized to post and
+    that you must have the correct object-level permissions
+    to delete a post.
+    """
+
+    def setUp(self):
+        # Create and log into a user
+        self.user = User.objects.create_user(
+            username='test-user', password='mock123it', email='me@mail.com')
+        self.client = APIClient()
+        response = self.client.post(
+            '/auth/', {'username': 'test-user', 'password': 'mock123it'})
+        self.token = json.loads(response.content)['token']
+
+    def test_authorized_listing(self):
+        Post.objects.create(message='Hello!', author=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token)
+        response = self.client.get('/posts/')
+        posts = json.loads(response.content)
+        self.assertEqual(len(posts), 1)
+
+    def test_unauthorized_listing(self):
+        Post.objects.create(message='Hello!', author=self.user)
+        response = self.client.get('/posts/')
+        posts = json.loads(response.content)
+        self.assertEqual(len(posts), 1)
+
+    def test_authorized_posting(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token)
+        post = 'Making a post on the wall!'
+        self.client.post('/posts/', {'message': post})
+        # The post should now exist
+        self.assertTrue(Post.objects.filter(message=post).exists())
+
+    def test_unauthorized_posting(self):
+        post = 'Making a post on the wall!'
+        self.client.post('/posts/', {'message': post})
+        # The post should not exist
+        self.assertFalse(Post.objects.filter(message=post).exists())
+
+    def test_authorized_deletion(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token)
+        post = 'Making a post on the wall!'
+        response = self.client.post('/posts/', {'message': post})
+        # Get the ID from the instance
+        instance = Post.objects.get(message=post)
+        self.client.delete('/posts/%i/' % instance.id)
+        # The post should no longer exist
+        self.assertFalse(Post.objects.filter(message=post).exists())
+
+    def test_unauthorized_deletion(self):
+        # Create a post with the original user
+        instance = Post.objects.create(message='Hello!', author=self.user)
+        # Create and log into a second user
+        newUser = User.objects.create_user(
+            username='test-user2', password='mock123it', email='me@mail.com')
+        response = self.client.post(
+            '/auth/', {'username': 'test-user2', 'password': 'mock123it'})
+        newToken = json.loads(response.content)['token']
+        # Now, try to delete with the new user's credentials
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + newToken)
+        self.client.delete('/posts/%i/' % instance.id)
+        # The post should still exist
+        self.assertTrue(Post.objects.filter(id=instance.id).exists())
+        # Now, totally deauthorize and try to delete
+        self.client.credentials(HTTP_AUTHORIZATION='')
+        self.client.delete('/posts/%i/' % instance.id)
+        # The post should still exist
+        self.assertTrue(Post.objects.filter(id=instance.id).exists())
--- site/users/tests.py
+++ site/users/tests.py
@@ -1,3 +1,64 @@
-from django.test import TestCase
+import json
+from unittest.mock import patch
 
-# Create your tests here.
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.test import TestCase
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APIClient
+
+from posts.models import Post
+
+from .models import User, send_success_email
+
+
+class UserTestCase(TestCase):
+    """ 
+    Testing that you can create a new user, 
+    login and logout (via token creation/destruction),
+    and that all new users receive a welcome email.
+    """
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_user_creation(self):
+        self.client.post(
+            '/users/', {'username': 'test-user', 'password': 'mock123it', 'email': 'me@mail.com'})
+        self.assertTrue(User.objects.filter(username='test-user').exists())
+
+    def test_user_login(self):
+        user = User.objects.create_user(
+            username='test-user', password='mock123it', email='me@mail.com')
+        self.client.post(
+            '/auth/', {'username': 'test-user', 'password': 'mock123it'})
+        self.assertTrue(Token.objects.filter(user=user).exists())
+
+    def test_user_logout(self):
+        user = User.objects.create_user(
+            username='test-user', password='mock123it', email='me@mail.com')
+        # Get a token for authorization
+        response = self.client.post(
+            '/auth/', {'username': 'test-user', 'password': 'mock123it'})
+        token = json.loads(response.content)['token']
+        # Logout using token
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token)
+        self.client.delete('/auth/')
+        # The token should no longer exist
+        self.assertFalse(Token.objects.filter(user=user).exists())
+
+    @patch('django.core.mail.EmailMessage.send')
+    def test_welcome_email(self, mockEmail):
+        # Connect the signal receiver
+        post_save.connect(send_success_email, sender=User)
+        # Create new user to trigger the signal
+        user = User.objects.create_user(
+            username='test-user', password='mock123it', email='me@mail.com')
+        # Welcome email should've been sent
+        self.assertTrue(mockEmail.called)
+        self.assertTrue(mockEmail.call_count, 1)
+        mockEmail.reset_mock()
+        # Now, update user
+        user.save()
+        # No further emails should've been sent
+        self.assertFalse(mockEmail.called)
