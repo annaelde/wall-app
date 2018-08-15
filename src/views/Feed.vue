98c3dec7e9075b3446e4797549cfabc88294a056
annaelde <elde.av@gmail.com>
2018-08-15 17:36:34 -0400

    handle a state without posts better.

--- src/views/Feed.vue
+++ src/views/Feed.vue
@@ -1,7 +1,12 @@
 <template>
     <section class="section home">
-        <div class="container">
+        <div v-if="posts && posts.length > 0" class="container">
             <post v-for="post in posts" :key="post.id" :post="post"></post>
+        </div>
+        <div v-else class="container">
+            <div class="notification is-info">
+                There's no posts to display!
+            </div>
         </div>
     </section>
 </template>
@@ -14,7 +19,7 @@ export default Vue.component('feed', {
     props: {
         posts: {
             type: Array,
-            required: true
+            required: false
         }
     }
 })
