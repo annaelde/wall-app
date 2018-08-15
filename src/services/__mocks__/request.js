f31445f944e1449e4b870de2cc2b81793bb754f9
annaelde <elde.av@gmail.com>
2018-08-15 17:37:36 -0400

    add request and post mocks.

---
+++ src/services/__mocks__/request.js
@@ -0,0 +1,11 @@
+const request = {
+    get: jest.fn().mockResolvedValue({ data: [] }),
+    post: jest.fn().mockResolvedValue({ data: [] }),
+    delete: jest.fn().mockResolvedValue({ data: [] })
+}
+
+const setToken = jest.fn()
+
+const removeToken = jest.fn()
+
+export { request, setToken, removeToken }
---
+++ tests/unit/resources/posts.js
@@ -0,0 +1,24 @@
+const posts = [{
+    id: 1,
+    message: 'First Message',
+    timestamp: '2018-08-15T19:27:34+00:00',
+    author: {
+        username: 'First Author'
+    }
+}, {
+    id: 2,
+    message: 'Second Message',
+    timestamp: '2018-08-15T19:27:34+00:00',
+    author: {
+        username: 'Second Author'
+    }
+}, {
+    id: 3,
+    message: 'Third Message',
+    timestamp: '2018-08-15T19:27:34+00:00',
+    author: {
+        username: 'Third Author'
+    }
+}]
+
+export { posts }
